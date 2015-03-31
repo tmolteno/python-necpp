c743db17e01b26cf10a1c0e9216f37415ee66192
Tim Molteno <tim@physics.otago.ac.nz>
2015-04-01 09:36:27 +1300

    Improved example code

--- example/optimized.py
+++ example/optimized.py
@@ -1,8 +1,8 @@
 import monopole
-
+import scipy.optimize
 import numpy as np
 
-# A function that will be minimized when the impedance is 50.0 Ohms
+# A function that will be minimized when the impedance is 50 Ohms
 def target(x):
   freq = 34.5
   z0 = np.exp(x[0]) # Make it positive
@@ -10,9 +10,10 @@ def target(x):
   z = monopole.impedance(freq, z0, height)
   return np.abs(z - 50.0)
   
-import scipy.optimize
 
 # Starting value 
 x0 = [-1.0, 1.5]
+
 z0, height = scipy.optimize.fmin(target, x0)
+
 print "Optimium z0=%fm, h=%fm, impedance=%s Ohms" % (np.exp(z0), np.exp(height), monopole.impedance(34.5, z0, height))
