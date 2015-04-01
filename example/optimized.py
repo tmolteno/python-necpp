05113de6c3f1b37745a97e39ba8da0990d5de011
Tim Molteno <tim@physics.otago.ac.nz>
2015-04-02 08:55:40 +1300

    Cleaned up optimization target

--- example/optimized.py
+++ example/optimized.py
@@ -3,17 +3,23 @@ import scipy.optimize
 import numpy as np
 
 # A function that will be minimized when the impedance is 50 Ohms
+# We convert the height and antenna length to positive
+# numbers using exp. because otherwise the antenna will lie
+# below ground and cause an error in simulation.
 def target(x):
   freq = 34.5
-  z0 = np.exp(x[0]) # Make it positive
-  height = np.exp(x[1])  # Make it positive
-  z = monopole.impedance(freq, z0, height)
+  height = np.exp(x[0]) # Make it positive
+  length = np.exp(x[1])  # Make it positive
+  z = monopole.impedance(freq, height, length)
   return np.abs(z - 50.0)
   
 
 # Starting value 
 x0 = [-1.0, 1.5]
+opt_height, opt_length = scipy.optimize.fmin(target, x0)
 
-z0, height = scipy.optimize.fmin(target, x0)
-
-print "Optimium z0=%fm, h=%fm, impedance=%s Ohms" % (np.exp(z0), np.exp(height), monopole.impedance(34.5, z0, height))
+ant_height = np.exp(opt_height)
+ant_length = np.exp(opt_length)
+ant_impedance = monopole.impedance(34.5, ant_height, ant_length)
+print "Tuned z0=%fm, h=%fm, impedance=%s Ohms" % \
+    (ant_height, ant_length, ant_impedance)
