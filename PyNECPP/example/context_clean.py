e00fa9941913542e0c1af5535643125a0a304664
Bart Coppens <kde@bartcoppens.be>
2015-11-15 18:20:00 +0100

    Cleaner geometry wrapping, thin wire kernel

--- PyNECPP/example/context_clean.py
+++ PyNECPP/example/context_clean.py
@@ -2,7 +2,7 @@
 
 # Currently these contain only the subset of cards that I needed
 
-class context_clean():
+class context_clean(object):
     def __init__(self, context):
         self.context = context
 
@@ -61,14 +61,31 @@ class context_clean():
 
         self.context.ex_card(ex_voltage_excitation, wire_tag, segment_nr, option_i3i4, voltage.real, voltage.imag, 0, 0, 0, 0)
 
+    def get_geometry(self):
+        #return geometry_clean(self.context.get_geometry()) # TODO
+        return self.context.get_geometry()
+
+    def set_extended_thin_wire_kernel(self, enable):
+        if enable:
+            self.context.set_extended_thin_wire_kernel(1)
+        else:
+            self.context.set_extended_thin_wire_kernel(0)
+
     # Some simple wrappers for context...
     # TODO: this should be simpler, can't this be auto-generated, or implicitly defined? The best solution is of course to do this in the C++ code,
     # and then the wrappers are immediately correct and nice
-    def get_geometry(self):
-        return self.context.get_geometry()
+
     def geometry_complete(self, *args):
         return self.context.geometry_complete(*args)
     def xq_card(self, *args):
         return self.context.xq_card(*args)
     def get_input_parameters(self, *args):
         return self.context.get_input_parameters(*args)
+
+class geometry_clean(object):
+    def __init__(self, geometry):
+        self.geometry = geometry
+
+    def wire(self, tag_id, nr_segments, src, dst, radius, length_ratio=1.0, radius_ratio=1.0):
+        """ radius is in meter. length_ratio can be set to have non-uniform segment lengths, radius_ratio can be used for tapered wires """
+        self.geometry.wire(tag_id, nr_segments, src[0], src[1], src[2], dst[0], dst[1], dst[2], radius, length_ratio, radius_ratio)
