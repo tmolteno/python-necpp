a48071ed06bb49538c5c7767d60c450d465ce1ff
Tim Molteno <tim@elec.ac.nz>
2026-06-27 08:28:26 +0200

    Fix #30/#16: Add float() casts and segment_count validation in context_clean.wire()
    - Cast all coordinates and radius to native float() to prevent SWIG
      TypeError with numpy scalars (#30: 'argument 4 of type nec_float')
    - Raise ValueError if segment_count < 2 (#16: NEC2 requires >= 2 segments)

--- PyNEC/example/context_clean.py
+++ PyNEC/example/context_clean.py
@@ -2,6 +2,7 @@
 
 # Currently these contain only the subset of cards that I needed
 
+
 class Range(object):
     def __init__(self, start, stop, count=None, delta=None):
         self.start = start
@@ -15,13 +16,16 @@ class Range(object):
         else:
             raise ValueError("Either count or delta must be specified")
 
+
 # Setting do_debug to True will dump all the cards generated with context_clean, so you can verify the output more easily in a text editor (and debug that file manually)
 do_debug = False
+
 
 def debug(card, *args):
     if do_debug:
         stringified = " , ".join([str(a) for a in args])
         print("%s %s" % (card, stringified))
+
 
 class context_clean(object):
     def __init__(self, context):
@@ -32,7 +36,7 @@ class context_clean(object):
         self.context.ld_card(ld_short_all_loads, 0, 0, 0, 0, 0, 0)
 
     def set_wire_conductivity(self, conductivity, wire_tag=None):
-        """ The conductivity is specified in mhos/meter. Currently all segments of a wire are set. If wire_tag is None, all wire_tags are set (i.e., a tag of 0 is used). """
+        """The conductivity is specified in mhos/meter. Currently all segments of a wire are set. If wire_tag is None, all wire_tags are set (i.e., a tag of 0 is used)."""
         if wire_tag is None:
             wire_tag = 0
 
@@ -43,8 +47,10 @@ class context_clean(object):
         self.set_wire_conductivity(conductivity)
 
     # TODO: multiplicative
-    def set_frequencies_linear(self, start_frequency, stop_frequency, count=None, step_size=None):
-        """ If start_frequency does not equal stop_frequency, either count or step should be specified. The other parameter will be automatically deduced """
+    def set_frequencies_linear(
+        self, start_frequency, stop_frequency, count=None, step_size=None
+    ):
+        """If start_frequency does not equal stop_frequency, either count or step should be specified. The other parameter will be automatically deduced"""
 
         if start_frequency == stop_frequency:
             step_size = 0
@@ -77,35 +83,68 @@ class context_clean(object):
         gn_finite_ground = 0
         no_ground_screen = 0
 
-        self.context.gn_card(gn_finite_ground, no_ground_screen, ground_dielectric, ground_conductivity, 0, 0, 0, 0)
+        self.context.gn_card(
+            gn_finite_ground,
+            no_ground_screen,
+            ground_dielectric,
+            ground_conductivity,
+            0,
+            0,
+            0,
+            0,
+        )
 
     def set_perfect_ground(self):
         gn_perfectly_conducting = 1
         no_ground_screen = 0
 
         debug("GN", gn_perfectly_conducting, no_ground_screen, 0, 0, 0, 0, 0, 0)
-        self.context.gn_card(gn_perfectly_conducting, no_ground_screen, 0, 0, 0, 0, 0, 0)
-
+        self.context.gn_card(
+            gn_perfectly_conducting, no_ground_screen, 0, 0, 0, 0, 0, 0
+        )
 
     # TODO: i1 = 5 is also a voltage excitation
     def voltage_excitation(self, wire_tag, segment_nr, voltage):
         ex_voltage_excitation = 0
-        no_action = 0 # TODO configurable
-        option_i3i4 = 10*no_action + no_action
+        no_action = 0  # TODO configurable
+        option_i3i4 = 10 * no_action + no_action
 
-        debug("EX", ex_voltage_excitation, wire_tag, segment_nr, option_i3i4, voltage.real, voltage.imag, 0, 0, 0, 0)
-        self.context.ex_card(ex_voltage_excitation, wire_tag, segment_nr, option_i3i4, voltage.real, voltage.imag, 0, 0, 0, 0)
+        debug(
+            "EX",
+            ex_voltage_excitation,
+            wire_tag,
+            segment_nr,
+            option_i3i4,
+            voltage.real,
+            voltage.imag,
+            0,
+            0,
+            0,
+            0,
+        )
+        self.context.ex_card(
+            ex_voltage_excitation,
+            wire_tag,
+            segment_nr,
+            option_i3i4,
+            voltage.real,
+            voltage.imag,
+            0,
+            0,
+            0,
+            0,
+        )
 
     def get_geometry(self):
-        #return geometry_clean(self.context.get_geometry()) # TODO
+        # return geometry_clean(self.context.get_geometry()) # TODO
         return self.context.get_geometry()
 
     def set_extended_thin_wire_kernel(self, enable):
         if enable:
-            debug ("EK", 0)
+            debug("EK", 0)
             self.context.set_extended_thin_wire_kernel(True)
         else:
-            debug ("EK", -1)
+            debug("EK", -1)
             self.context.set_extended_thin_wire_kernel(False)
 
     def geometry_complete(self, ground_plane, current_expansion=True):
@@ -141,22 +180,65 @@ class context_clean(object):
     average_todo = 2
 
     # TODO: this should be different for surface_wave_mode (1), because then thetas = z
-    def radiation_pattern(self, thetas, phis, output_mode=output_vertical_horizontal, normalization=normalization_none, gain=power_gain, average=average_todo):
-        """ thetas and phis should be Range(-like) objects """
-        normal_mode = 0 # TODO other modes
+    def radiation_pattern(
+        self,
+        thetas,
+        phis,
+        output_mode=output_vertical_horizontal,
+        normalization=normalization_none,
+        gain=power_gain,
+        average=average_todo,
+    ):
+        """thetas and phis should be Range(-like) objects"""
+        normal_mode = 0  # TODO other modes
 
         # the rp_card already has XNDA as separate arguments
-        radial_distance = 0 # TODO
-        gnornamize_maximum = 0 # TODO
+        radial_distance = 0  # TODO
+        gnornamize_maximum = 0  # TODO
 
-        xnda = average + 10*gain+100*normalization+1000*output_mode
+        xnda = average + 10 * gain + 100 * normalization + 1000 * output_mode
 
-        debug("RP", normal_mode, thetas.count, phis.count, xnda, thetas.start, phis.start, thetas.delta, phis.delta, radial_distance, gnornamize_maximum)
-        self.context.rp_card(normal_mode, thetas.count, phis.count, output_mode, normalization, gain, average, thetas.start, phis.start, thetas.delta, phis.delta, radial_distance, gnornamize_maximum)
+        debug(
+            "RP",
+            normal_mode,
+            thetas.count,
+            phis.count,
+            xnda,
+            thetas.start,
+            phis.start,
+            thetas.delta,
+            phis.delta,
+            radial_distance,
+            gnornamize_maximum,
+        )
+        self.context.rp_card(
+            normal_mode,
+            thetas.count,
+            phis.count,
+            output_mode,
+            normalization,
+            gain,
+            average,
+            thetas.start,
+            phis.start,
+            thetas.delta,
+            phis.delta,
+            radial_distance,
+            gnornamize_maximum,
+        )
 
     # TODO: shunt admittances, length of transmission line if not straight-line distance
-    def transmission_line(self, src, dst, impedance, crossed_line=False, length=None, shunt_admittance_src=0, shunt_admittance_dst=0):
-        """ src and dst are (tag_nr, segment_nr) pairs """
+    def transmission_line(
+        self,
+        src,
+        dst,
+        impedance,
+        crossed_line=False,
+        length=None,
+        shunt_admittance_src=0,
+        shunt_admittance_dst=0,
+    ):
+        """src and dst are (tag_nr, segment_nr) pairs"""
         if crossed_line:
             impedance *= -1
         if length is None:
@@ -164,23 +246,70 @@ class context_clean(object):
         shunt_admittance_src = complex(shunt_admittance_src)
         shunt_admittance_dst = complex(shunt_admittance_dst)
 
-        debug("TL", src[0], src[1], dst[0], dst[1], impedance, length, shunt_admittance_src.real, shunt_admittance_src.imag, shunt_admittance_dst.real, shunt_admittance_dst.imag)
-        self.context.tl_card(src[0], src[1], dst[0], dst[1], impedance, length, shunt_admittance_src.real, shunt_admittance_src.imag, shunt_admittance_dst.real, shunt_admittance_dst.imag)
+        debug(
+            "TL",
+            src[0],
+            src[1],
+            dst[0],
+            dst[1],
+            impedance,
+            length,
+            shunt_admittance_src.real,
+            shunt_admittance_src.imag,
+            shunt_admittance_dst.real,
+            shunt_admittance_dst.imag,
+        )
+        self.context.tl_card(
+            src[0],
+            src[1],
+            dst[0],
+            dst[1],
+            impedance,
+            length,
+            shunt_admittance_src.real,
+            shunt_admittance_src.imag,
+            shunt_admittance_dst.real,
+            shunt_admittance_dst.imag,
+        )
 
     # Some simple wrappers for context...
     # TODO: this should be simpler, can't this be auto-generated, or implicitly defined? The best solution is of course to do this in the C++ code,
     # and then the wrappers are immediately correct and nice
     def xq_card(self, *args):
         return self.context.xq_card(*args)
+
     def get_input_parameters(self, *args):
         return self.context.get_input_parameters(*args)
+
 
 class geometry_clean(object):
     def __init__(self, geometry):
         self.geometry = geometry
 
-    def wire(self, tag_id, nr_segments, src, dst, radius, length_ratio=1.0, radius_ratio=1.0):
-        """ radius is in meter. length_ratio can be set to have non-uniform segment lengths, radius_ratio can be used for tapered wires """
-        debug("GW", tag_id, nr_segments, src[0], src[1], src[2], dst[0], dst[1], dst[2], radius) # TODO
+    def wire(
+        self, tag_id, nr_segments, src, dst, radius, length_ratio=1.0, radius_ratio=1.0
+    ):
+        """radius is in meter. length_ratio can be set to have non-uniform segment lengths, radius_ratio can be used for tapered wires"""
+        if nr_segments < 2:
+            raise ValueError(
+                f"segment_count must be >= 2 (got {nr_segments}). NEC2 requires at least 2 segments per wire."
+            )
+        # Cast to native float in case numpy scalars are passed (SWIG may not convert them)
+        xw1, yw1, zw1 = float(src[0]), float(src[1]), float(src[2])
+        xw2, yw2, zw2 = float(dst[0]), float(dst[1]), float(dst[2])
+        rad = float(radius)
+        debug("GW", tag_id, nr_segments, xw1, yw1, zw1, xw2, yw2, zw2, rad)  # TODO
 
-        self.geometry.wire(tag_id, nr_segments, src[0], src[1], src[2], dst[0], dst[1], dst[2], radius, length_ratio, radius_ratio)
+        self.geometry.wire(
+            tag_id,
+            nr_segments,
+            xw1,
+            yw1,
+            zw1,
+            xw2,
+            yw2,
+            zw2,
+            rad,
+            float(length_ratio),
+            float(radius_ratio),
+        )
